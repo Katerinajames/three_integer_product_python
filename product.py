d04a8bb4d444fd9995a3624e9b69bf3638a319a7
Katerina <karitsa34@yahoo.gr>
2022-05-19 22:17:55 +0300

    Three integer product

---
+++ product.py
@@ -0,0 +1,8 @@
+print("Three number product")
+
+
+n=int(input("Enter a number\n"))
+n2=int(input("Enter a second number\n"))
+n3=int(input("Enter a third number\n"))
+
+print ("Our product is  ",n*n2*n3)
